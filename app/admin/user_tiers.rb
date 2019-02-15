fe7447ea45b6bfef64507bffbd54091ee02cc8aa
Fidenz-kaushiks <kaushik.s@fidenz.com>
2019-02-15 14:00:28 +0530

    version update

--- app/admin/user_tiers.rb
+++ app/admin/user_tiers.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register UserTier, as: 'Subscription' do
+  config.filters = false
   actions :all, except: [:new, :destroy, :edit]
 
   index do
@@ -6,6 +7,7 @@ ActiveAdmin.register UserTier, as: 'Subscription' do
     column :user
     column :name
     column :price
+    column (:max_horses) { |tier| tier.horses }
     column :created_at
     actions
   end
@@ -15,6 +17,7 @@ ActiveAdmin.register UserTier, as: 'Subscription' do
       rows :user
       rows :name
       rows :price
+      rows :horses
       rows :created_at
     end
   end
